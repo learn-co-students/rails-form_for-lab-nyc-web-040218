9128a6720c1e5616be8098294e6d4e7b23180007
briecodes <hello@brie.work>
2018-04-28 21:58:49 -0400

    Paths created for both students and schoolclasses.

--- app/controllers/schoolclasses_controller.rb
+++ app/controllers/schoolclasses_controller.rb
@@ -2,16 +2,28 @@ class SchoolclassesController < ApplicationController
     def index
         @schoolclasses = Schoolclass.all
     end
-    
+
     def new
     end
 
     def create
     end
 
+    def edit
+        @schoolclass = find_class(params[:id])
+    end
+
     def show
+        @schoolclass = find_class(params[:id])
     end
 
     def update
+        @schoolclass = find_class(params[:id])
+        # @schoolclass = Schoolclass.find(params[:id])
     end
+
+    private
+        def find_class(param)
+            Schoolclass.find(param)
+        end
 end
