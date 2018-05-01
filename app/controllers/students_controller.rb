33f80f15e9046d64da0fda51817feab75889ef8e
Ryan Hu <ryanhuuu@gmail.com>
2018-05-01 13:04:48 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,36 @@
+require 'byebug'
+class StudentsController < ActionController::Base
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def create
+
+    @student = Student.new(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to student_path(@student)
+
+  end
+
+
+
+end
